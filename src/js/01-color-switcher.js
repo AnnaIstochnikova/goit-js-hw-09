41be07cf1461fdf81c1b0e4657f1132189b69947
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-08-25 10:14:05 +0200

    push first part

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+'use strict';
 
+const startBtn = document.querySelector('.button--start');
+const stopBtn = document.querySelector('.button--stop');
+const body = document.querySelector('body');
+let timerId = null;
+console.log(body);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+});
